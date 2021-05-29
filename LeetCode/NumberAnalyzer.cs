9ab401fec4de4b979cc0c88bd76bc9d2f6a69c68
Rokas Sidaravičius <52298228+untuned1337@users.noreply.github.com>
2021-05-30 00:46:05 +0300

    refactor: Moved number helper methods to a seperate helper class

---
+++ LeetCode/NumberAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Easy.AddDigits
+{
+    public static class NumberAnalyzer
+    {
+        public static int RecursivelyAddDigitsUntilOneDigitNumber(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+
+            var digits = GetDigits(number);
+            if (digits.Count > 1)
+            {
+                var sumOfDigits = AddDigits(digits);
+                return RecursivelyAddDigitsUntilOneDigitNumber(sumOfDigits);
+            }
+
+            return digits[0];
+        }
+
+        public static List<int> GetDigits(int number)
+        {
+            var digits = new List<int>();
+            while (number > 0)
+            {
+                var nextDigit = number % 10;
+                number /= 10;
+                digits.Add(nextDigit);
+            }
+
+            return digits;
+        }
+
+        public static int AddDigits(List<int> numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Easy.AddDigits;
+using System;
 
 namespace LeetCode
 {
@@ -7,49 +7,8 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var result = RecursivelyAddDigitsUntilOneDigitNumber(68);
+            var result = NumberAnalyzer.RecursivelyAddDigitsUntilOneDigitNumber(68);
             Console.WriteLine(result);
         }
-
-        public static int RecursivelyAddDigitsUntilOneDigitNumber(int number) 
-        {
-            if (number == 0)
-            {
-                return 0;
-            }
-
-            var digits = GetDigits(number);
-            if (digits.Count > 1)
-            {
-                var sumOfDigits = AddDigits(digits);
-                return RecursivelyAddDigitsUntilOneDigitNumber(sumOfDigits);
-            }
-            
-            return digits[0];
-        }
-
-        private static List<int> GetDigits(int number)
-        {
-            var digits = new List<int>();
-            while (number > 0)
-            {
-                var nextDigit = number % 10;
-                number /= 10;
-                digits.Add(nextDigit);
-            }
-
-            return digits;
-        }
-
-        private static int AddDigits(List<int> numbers)
-        {
-            var sum = 0;
-            foreach (var number in numbers)
-            {
-                sum += number;
-            }
-
-            return sum;
-        } 
     }
 }
