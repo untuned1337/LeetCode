0e2a08a75be8962434b282472cdc46efcf166b66
Rokas Sidaravičius <52298228+untuned1337@users.noreply.github.com>
2021-06-02 22:37:46 +0300

    feature: Solved Easy problem "1185. Day of the Week"

---
+++ Easy.DayOfTheWeek/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Easy.DayOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDayOfTheWeek(31, 8, 2019));
+        }
+
+        public static string GetDayOfTheWeek(int day, int month, int year)
+        {
+            var dateTime = new DateTime(year, month, day);
+            return dateTime.DayOfWeek.ToString();
+        }
+    }
+}
